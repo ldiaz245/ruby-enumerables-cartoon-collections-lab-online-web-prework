30097b2511e5db0b547e66f001f440e1fd24d604
LUIS J Alvarez Diaz <ldiaz244@gmail.com>
2019-08-06 16:02:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,8 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves# ["doc","dopey","Bashful", "Grumpy"]
+  "Doc"
+  "Dopey"
+  "Bashful"
+  "Grumpy"
 end
 
 def summon_captain_planet# code an argument here
